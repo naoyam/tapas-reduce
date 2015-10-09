920d9bf352c5705b1a8e2ca6963d1278862f8793
Keisuke Fukuda <fukuda@matsulab.is.titech.ac.jp>
2015-10-09 14:16:43 +0900

    try to run setup-travis.sh as before_script on Travis

--- setup-travis.sh
+++ setup-travis.sh
@@ -2,15 +2,7 @@
 
 # things to do for travis-ci in the before_install section
 
-if ( test "`uname -s`" = "Darwin" )
-then
-  #cmake v2.8.12 is installed on the Mac workers now
-  #brew update
-  #brew install cmake
-  echo
-else
-  #install a newer cmake since at this time Travis only has version 2.8.7
-  echo "yes" | sudo add-apt-repository ppa:kalakris/cmake
-  sudo apt-get update -qq
-  sudo apt-get install cmake
-fi
+#install a newer cmake since at this time Travis only has version 2.8.7
+sudo add-apt-repository --yes ppa:kalakris/cmake
+sudo apt-get update -qq
+sudo apt-get install cmake
